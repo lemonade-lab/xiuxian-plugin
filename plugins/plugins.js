41219334d6da11de12d2e17a450fb4df85e8dbf5
916415899@qq.com <916415899@qq.com>
2022-11-08 14:17:56 +0800

    V1.3分布式

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,7 +1,4 @@
 import plugin from '../../../lib/plugins/plugin.js'
-console.log("-------------");
-console.log("[修仙插件模块]初始化~");
-console.log("-------------");
 export class plugins extends plugin {
     constructor() {
         super({
