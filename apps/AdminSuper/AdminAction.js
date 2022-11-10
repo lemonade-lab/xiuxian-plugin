754f6ed1b654877aa72594625c64363c50c85f3e
916415899@qq.com <916415899@qq.com>
2022-11-10 13:04:00 +0800

    V2.0优化

--- apps/AdminSuper/AdminAction.js
+++ apps/AdminSuper/AdminAction.js
@@ -17,10 +17,88 @@ export class AdminAction extends plugin {
                 {
                     reg: "^#修仙更新",
                     fnc: "checkout",
+                },
+                {
+                    reg: "^#修仙强制更新",
+                    fnc: "forcecheckout",
                 }
             ],
         });
         this.key = "xiuxian:restart";
+    }
+
+    async forcecheckout(e){
+        if (!e.isMaster) {
+            return;
+        }
+        let msg = ["————[更新消息]————"];
+        let command = "git fetch --all && git reset --hard main && git  pull";
+        msg.push("正在更新...");
+        const that = this;
+        exec(
+            command,
+            { cwd: `${_path}/plugins/xiuxian-emulator-plugin/` },
+            function (error, stdout, stderr) {
+                if (/(Already up[ -]to[ -]date|已经是最新的)/.test(stdout)) {
+                    msg.push("最新版修仙插件了~");
+                    ForwardMsg(e, msg);
+                    return;
+                }
+                if (error) {
+                    msg.push(
+                        "更新失败！\nError code: " +
+                        error.code +
+                        "\n" +
+                        error.stack +
+                        "\n 请稍后重试..."
+                    );
+                    ForwardMsg(e, msg);
+                    return;
+                }
+                msg.push("更新成功，正在重启更新...");
+                timer && clearTimeout(timer);
+                timer = setTimeout(async () => {
+                    try {
+                        let data = JSON.stringify({
+                            isGroup: !!e.isGroup,
+                            id: e.isGroup ? e.group_id : e.user_id,
+                        });
+                        await redis.set(that.key, data, { EX: 120 });
+                        let cm = "npm run start";
+                        if (process.argv[1].includes("pm2")) {
+                            cm = "npm run restart";
+                        } else {
+                            msg.push("正在转为后台运行...");
+                        }
+                        exec(cm, (error, stdout, stderr) => {
+                            if (error) {
+                                redis.del(that.key);
+                                msg.push(
+                                    "重启失败了\nError code: " +
+                                    error.code +
+                                    "\n" +
+                                    error.stack +
+                                    "\n"
+                                );
+                                logger.error(`重启失败\n${error.stack}`);
+                            } else if (stdout) {
+                                logger.mark("重启成功，运行已转为后台");
+                                logger.mark("查看日志请用命令：npm run log");
+                                logger.mark("停止后台运行命令：npm stop");
+                                process.exit();
+                            }
+                        });
+                    } 
+                    catch (error) {
+                        redis.del(that.key);
+                        let e = error.stack ?? error;
+                        msg.push("重启失败了\n" + e);
+                    }
+                }, 1000);
+                ForwardMsg(e, msg);
+            }
+        );
+        return true;
     }
 
     async checkout(e) {
