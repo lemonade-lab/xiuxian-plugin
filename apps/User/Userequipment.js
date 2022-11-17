ecde8c7c6a02e97e960c292b3346ab285b758598
916415899@qq.com <916415899@qq.com>
2022-11-17 22:34:57 +0800

    V2.0优化

--- apps/User/Userequipment.js
+++ apps/User/Userequipment.js
@@ -1,7 +1,5 @@
 import plugin from '../../../../lib/plugins/plugin.js'
-import {
-    existplayer, search_thing_name, exist_najie_thing_id,Read_najie, 
-    Read_equipment, Write_equipment, Write_najie,Add_najie_thing} from '../Xiuxian/Xiuxian.js'
+import {existplayer, search_thing_name, exist_najie_thing_id,Read_najie,Read_equipment, Write_equipment, Write_najie,Add_najie_thing} from '../Xiuxian/Xiuxian.js'
 export class Userequipment extends plugin {
     constructor() {
         super({
@@ -23,90 +21,87 @@ export class Userequipment extends plugin {
                     fnc: 'upgrade_equipment'
                 }
             ]
-        })
-    }
-
+        });
+    };
     async upgrade_equipment(e){
         if (!e.isGroup) {
             return;
-        }
+        };
         e.reply("待更新");
         return;
-    }
-
+    };
     async add_equipment(e) {
         if (!e.isGroup) {
             return;
-        }
-        let usr_qq = e.user_id;
-        let ifexistplay = await existplayer(usr_qq);
+        };
+        const usr_qq = e.user_id;
+        const ifexistplay = await existplayer(usr_qq);
         if (!ifexistplay) {
             return;
-        }
-        let thing_name = e.msg.replace("#装备", '');
-        let searchsthing = await search_thing_name(thing_name);
+        };
+        const thing_name = e.msg.replace("#装备", '');
+        const searchsthing = await search_thing_name(thing_name);
         if (searchsthing == 1) {
             e.reply("世界没有" + thing_name);
             return;
-        }
-        let najie_thing = await exist_najie_thing_id(usr_qq, searchsthing.id);
+        };
+        const najie_thing = await exist_najie_thing_id(usr_qq, searchsthing.id);
         if (najie_thing == 1) {
             e.reply("没有" + thing_name);
             return;
-        }
-        let id=searchsthing.id.split('-')
+        };
+        const id=searchsthing.id.split('-')
         if(id[0]!= 1 ){
             if(id[0]!=2){
                 if (id[0]!=3){
                     e.reply(`此物无法被装备`);
                     return ;
-                }
-            }
-        }
-        let equipment = await Read_equipment(usr_qq);
+                };
+            };
+        };
+        const equipment = await Read_equipment(usr_qq);
         if (equipment.length < 4) {
             equipment.push(searchsthing);
             await Write_equipment(usr_qq, equipment);
-        } else {
+        } 
+        else {
             e.reply("无法操控更多装备");
             return;
-        }
-        let najie = await Read_najie(usr_qq);
+        };
+        const najie = await Read_najie(usr_qq);
         najie = await Add_najie_thing(najie, searchsthing, -1);
         await Write_najie(usr_qq, najie);
         e.reply("装备" + thing_name);
         return;
-    }
-
+    };
     async delete_equipment(e) {
         if (!e.isGroup) {
             return;
-        }
-        let usr_qq = e.user_id;
-        let ifexistplay = await existplayer(usr_qq);
+        };
+        const usr_qq = e.user_id;
+        const ifexistplay = await existplayer(usr_qq);
         if (!ifexistplay) {
             return;
-        }
-        let thing_name = e.msg.replace("#卸下", '');
-        let equipment = await Read_equipment(usr_qq);
-
-        let islearned = equipment.find(item => item.name == thing_name);
+        };
+        const thing_name = e.msg.replace("#卸下", '');
+        const equipment = await Read_equipment(usr_qq);
+        const islearned = equipment.find(item => item.name == thing_name);
         if (islearned) {
             equipment = equipment.filter(item => item.name != thing_name);
             await Write_equipment(usr_qq, equipment);
-        } else {
+        } 
+        else {
             e.reply("未装备");
-        }
-        let searchsthing = await search_thing_name(thing_name);
+        };
+        const searchsthing = await search_thing_name(thing_name);
         if (searchsthing == 1) {
             e.reply("世界没有" + thing_name);
             return;
-        }
-        let najie = await Read_najie(usr_qq);
+        };
+        const najie = await Read_najie(usr_qq);
         najie = await Add_najie_thing(najie, searchsthing, 1);
         await Write_najie(usr_qq, najie);
         e.reply("已卸下" + thing_name);
         return;
-    }
-   
-}
+    };
+};
