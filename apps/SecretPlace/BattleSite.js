c19e1e14e9d3add511d02848fc8cb275e333026f
matianhao <matianhao@tvjoy.com>
2022-11-19 10:03:45 +0800

    物品掉落修改

--- apps/SecretPlace/BattleSite.js
+++ apps/SecretPlace/BattleSite.js
@@ -1,8 +1,9 @@
 import plugin from '../../../../lib/plugins/plugin.js';
 import data from '../../model/XiuxianData.js';
+import fs from "node:fs";
 import Cachemonster from "../../model/cachemonster.js";
-import { Gomini, Read_action, ForwardMsg, Read_battle, monsterbattle, Add_experiencemax, Add_experience, Add_lingshi } from '../Xiuxian/Xiuxian.js';
-import {Add_najie_thing, Read_najie, Write_najie} from "../Xiuxian/Xiuxian";
+import { Gomini, Read_action, ForwardMsg, Read_battle, monsterbattle, Add_experiencemax, Add_experience, Add_lingshi,GenerateCD,Add_najie_thing, Read_najie, Write_najie } from '../Xiuxian/Xiuxian.js';
+
 export class BattleSite extends plugin {
     constructor() {
         super({
@@ -27,21 +28,22 @@ export class BattleSite extends plugin {
         if (!good) {
             return;
         };
+        const usr_qq = e.user_id;
         const CDid = "10";
         const now_time = new Date().getTime();
         const CDTime = 5;
-        const CD = await GenerateCD(A, CDid);
+        const CD = await GenerateCD(usr_qq, CDid);
         if (CD != 0) {
             e.reply(CD);
+            return ;
         };
-        const usr_qq = e.user_id;
         const name = e.msg.replace("#击杀", '');
         const action = await Read_action(usr_qq);
         //非安全区判断
         const p = await Cachemonster.monsters(action.x, action.y, action.z);
         if (p != -1) {
-            await redis.set("xiuxian:player:" + A + ':' + CDid, now_time);
-            await redis.expire("xiuxian:player:" + A + ':' + CDid, CDTime * 60);
+            await redis.set("xiuxian:player:" + usr_qq + ':' + CDid, now_time);
+            await redis.expire("xiuxian:player:" + usr_qq + ':' + CDid, CDTime * 60);
             const monstersdata = await Cachemonster.monsterscache(p);
             const mon=monstersdata.find(item => item.name == name);
             const LevelMax = data.LevelMax_list.find(item => item.id == mon.level);
@@ -63,13 +65,13 @@ export class BattleSite extends plugin {
                 const m=Math.floor((Math.random() * (100-1))) + Number(1);
                 //获得装备
                 if(m<mon.level*5){
-                    msg.push(usr_qq+"得到了装备");
-                    const dropsItemList = JSON.parse(fs.readFileSync(`${data.dropsItem}/dropsItem.json`));
+                    const dropsItemList = JSON.parse(fs.readFileSync(`${data.all}/dropsItem.json`));
                     const length = dropsItemList.length;
                     const random = Math.floor(Math.random() * length);
                     let nacre = await Read_najie(usr_qq);
                     nacre = await Add_najie_thing(nacre, dropsItemList[random], 1);
                     await Write_najie(usr_qq, nacre);
+                    msg.push(usr_qq+`得到了装备[${dropsItemList[random].name}]`);
                 }
                 //获得气血
                 else if(m<mon.level*8){
@@ -81,7 +83,9 @@ export class BattleSite extends plugin {
                     msg.push(usr_qq+"得到99灵石和99修为");
                     await Add_experience(usr_qq,99);
                     await Add_lingshi(usr_qq,99);
-                };
+                }else {
+                    msg.push(usr_qq+"运气不好，什么也没得到");
+                }
                 await ForwardMsg(e, msg);
                 return;
             };
