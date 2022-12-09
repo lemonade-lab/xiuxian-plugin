b76942f83d3a321d432128073c483247597a429b
916415899@qq.com <916415899@qq.com>
2022-12-09 19:15:25 +0800

    优化

--- apps/AdminSuper/AdminPlugins.js
+++ apps/AdminSuper/AdminPlugins.js
@@ -175,18 +175,4 @@ export class AdminPlugins extends plugin {
         );
         return true;
     };
-
-    init = async () => {
-        const the = { restart: '' };
-        the.restart = await redis.get(this.key);
-        if (the.restart) {
-            the.restart = JSON.parse(the.restart);
-            if (the.restart.isGroup) {
-                Bot.pickGroup(the.restart.id).sendMsg('重启成功!\n【#修仙版本】\n以确保正常使用\n');
-            } else {
-                Bot.pickGroup(the.restart.id).sendMsg('重启成功!\n【#修仙版本】\n以确保正常使用\n');
-            }
-            redis.del(this.key);
-        };
-    };
 };
