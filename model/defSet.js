7b6225c95f60126e25910980b791357a539ddb15
916415899@qq.com <916415899@qq.com>
2022-12-10 18:27:46 +0800

    优化

--- model/defSet.js
+++ model/defSet.js
@@ -2,7 +2,8 @@ import fs from 'node:fs';
 import path from 'path';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const __dirname = `${path.resolve()}${path.sep}config${path.sep}config/other.yaml`;
+const other = `${path.resolve()}${path.sep}config${path.sep}config/other.yaml`;
+const group = `${path.resolve()}${path.sep}config${path.sep}config/group.yaml`;
 class defSet {
   constructor() {
     try {
@@ -14,11 +15,11 @@ class defSet {
   };
   ReadConfig = () => {
     try {
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(group);
       const sum = ['十连', '角色查询', '体力查询', '用户绑定', '抽卡记录', '添加表情', '欢迎新人', '退群通知', '云崽帮助', '角色素材', '今日素材', '养成计算', '米游社公告']
       data.default.disable.push(...sum);
       const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      fs.writeFileSync(group, yamlStr, 'utf8');
       return '关闭成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
@@ -26,11 +27,11 @@ class defSet {
   };
   ReadConfighelp = () => {
     try {
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(group);
       const sum = ['云崽帮助']
       data.default.disable.push(...sum);
-      const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      const yamlStr = this.yaml.dump(data);0
+      fs.writeFileSync(group, yamlStr, 'utf8');
       return '设置成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
@@ -39,11 +40,11 @@ class defSet {
   AddMaster = (mastername) => {
     try {
       const QQ = Number(mastername);
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(other);
       const sum = [QQ];
       data.masterQQ.push(...sum);
       const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      fs.writeFileSync(other, yamlStr, 'utf8');
       return '添加成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
@@ -52,7 +53,7 @@ class defSet {
   DeleteMaster = (mastername) => {
     try {
       const QQ = Number(mastername);
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(other);
       const sum = [];
       data.masterQQ.forEach((item) => {
         if (item != QQ) {
@@ -61,7 +62,7 @@ class defSet {
       });
       data.masterQQ = sum;
       const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      fs.writeFileSync(other, yamlStr, 'utf8');
       return '删除成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
@@ -69,10 +70,10 @@ class defSet {
   };
   OffGroup = () => {
     try {
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(other);
       data.disablePrivate = true;
       const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      fs.writeFileSync(other, yamlStr, 'utf8');
       return '关闭成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
@@ -80,10 +81,10 @@ class defSet {
   };
   OnGroup = () => {
     try {
-      const data = this.YAML.load(__dirname);
+      const data = this.YAML.load(other);
       data.disablePrivate = false;
       const yamlStr = this.yaml.dump(data);
-      fs.writeFileSync(__dirname, yamlStr, 'utf8');
+      fs.writeFileSync(other, yamlStr, 'utf8');
       return '开启成功';
     } catch {
       return '请先执行\npnpm i yamljs -w\npnpm i  js-yaml -w';
