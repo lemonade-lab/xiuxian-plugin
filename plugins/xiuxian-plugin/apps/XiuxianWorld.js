2cc1d371d01a3e2f48d1459b9c3c9922e9e712ef
916415899@qq.com <916415899@qq.com>
2022-12-12 01:27:45 +0800

    优化

--- plugins/xiuxian-plugin/apps/XiuxianWorld.js
+++ plugins/xiuxian-plugin/apps/XiuxianWorld.js
@@ -32,5 +32,6 @@ export class XiuxianWorld extends plugin {
         };
         const plain='[V1.2升级V2.0]\n1.同时安装V1.2与V2.0\n2.#修仙数据升级\nV2.0已有存档的玩家\n将会同步V1.2数据';
         e.reply(plain);
+        return;
     }
 };
