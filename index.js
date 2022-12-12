56fddbf0b8aab3ad4e20e9e1896b2851649801ab
916415899@qq.com <916415899@qq.com>
2022-12-12 12:07:12 +0800

    优化

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Config from './model/Config.js';
 import index from './model/index.js';
-import Xiuxianschedule from './model/Xiuxianschedule.js';
+import Xiuxianschedule from './model/XiuxianSchedule.js';
 Xiuxianschedule.scheduleJobflie('0 0 */1 * * ?');
 const versionData = Config.getdefSet('version', 'version');
 const xiuxain = await index.toindex('apps');
