1a10d456d582a4c1ccce59e13906c31012a10c8a
916415899@qq.com <916415899@qq.com>
2023-01-09 12:15:09 +0800

    优化

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import Config from './model/Config.js';
 import index from './model/index.js';
 import Xiuxianschedule from './model/XiuxianSchedule.js';
 Xiuxianschedule.scheduleJobflie('0 0 */1 * * ?');
-const versionData = Config.getdefSet('version', 'version');
 const xiuxain = await index.toindex('apps');
 const plugin = await index.toindex('plugins');
 const apps = { ...xiuxain, ...plugin };
-logger.info(`Xiuxian-Plugin-Box[V${versionData[0].version}]`);
+logger.info(`Xiuxian-Plugin-Box[2022-12-01]`);
 export { apps }; 
