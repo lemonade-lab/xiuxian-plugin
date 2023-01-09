2520433911dae509d9226d70db3d4f51592e5f3d
916415899@qq.com <916415899@qq.com>
2023-01-09 11:15:06 +0800

    优化

--- model/help.js
+++ model/help.js
@@ -2,31 +2,28 @@ import xiuxianCfg from './Config.js';
 const appname = 'Xiuxian-Plugin-Box'
 const dirname = `plugins/${appname}/resources`
 class Help {
-  constructor() {
-    this._path = process.cwd().replace(/\\/g, '/')  //插件位置 
-  }
-  gethelp = async (e, folename) => {
-    const helpData = xiuxianCfg.getConfig('help', folename);
+  gethelp = async (e, filename) => {
+    const helpData = xiuxianCfg.getConfig('help', filename);
     return {
       saveId: 'help',//这里其实是文件名
       //模板html路径  
       tplFile: `./${dirname}/html/help/help.html`,
       /** 绝对路径 */
-      pluResPath: `${this._path}/${dirname}/`,
+      pluResPath: `${process.cwd().replace(/\\/g, '/')}/${dirname}/`,
       /** 版本 */
       version: "v2.0",
       /** 数据 */
       helpData
     }
   }
-  getboxhelp = async (folename) => {
-    const helpData = xiuxianCfg.getConfig('help', folename);
+  getboxhelp = async (filename) => {
+    const helpData = xiuxianCfg.getConfig('help', filename);
     return {
       saveId: 'help',//这里其实是文件名
       //模板html路径  
       tplFile: `./${dirname}/html/help/help.html`,
       /** 绝对路径 */
-      pluResPath: `${this._path}/${dirname}/`,
+      pluResPath: `${process.cwd().replace(/\\/g, '/')}/${dirname}/`,
       /** 版本 */
       version: "v2.0",
       /** 数据 */
--- model/show.js
+++ model/show.js
@@ -1,11 +1,6 @@
 const appname = 'Xiuxian-Plugin-Box'
 const dirname = `plugins/${appname}/resources`
 class Show {
-  constructor() {
-    this.userId = ''  //文件标记
-    this.name = ''    //地址标记
-    this._path = process.cwd().replace(/\\/g, '/')  //插件位置 
-  }
   /**
    * @param {地址} path 
    * @param {文件名} name 
@@ -14,18 +9,17 @@ class Show {
    * @returns 
    */
   get_Data = async (path, name, myData, UID) => {
-    this.userId = UID  //文件名
-    this.path = path//地址
-    this.name = name//文件名
+    const userId = UID  //用户id
+    const htmlname=name = name//文件名
     return {
       //html保存id
-      saveId: this.userId,
-      //模板html路径                    //
-      tplFile: `./${dirname}/html/${this.path}/${this.name}.html`,
+      saveId: userId,
+      //模板html路径  
+      tplFile: `./${dirname}/html/${path}/${htmlname}.html`,
       /** 绝对路径 */
-      //插件资源路径
-      pluResPath: `${this._path}/${dirname}/`,
-      ...myData,//数据
+      pluResPath: `${process.cwd().replace(/\\/g, '/')}/${dirname}/`,
+      /** 数据 */
+      ...myData,
     }
   }
 }
