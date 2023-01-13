e52da6371fb190f39840d11a88e4c98b4f07ac23
916415899@qq.com <916415899@qq.com>
2023-01-13 12:14:36 +0800

    优化

--- apps/boxceshi.js
+++ apps/boxceshi.js
@@ -6,13 +6,15 @@ export class boxceshi extends robotapi {
     constructor() {
         super(superIndex([
             {
-                reg: '^#测试$',
+                reg: '^#修仙测试$',
                 fnc: 'ceshi'
             }
         ]))
     }
     ceshi = async(e)=>{
-        //输入一个地址
+        if (!e.isMaster) {
+            return
+        }
         const name=boxfs.returnMenu(__dirname)
         name.forEach((item)=>{
             e.reply(item)
