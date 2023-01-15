bf4d939f35415aed97912bde305e4a6a209bc45f
916415899@qq.com <916415899@qq.com>
2023-01-15 19:52:20 +0800

    优化

---
+++ model/robot/rebotapi.js
@@ -0,0 +1,21 @@
+import plugin from '../../../../lib/plugins/plugin.js'
+export default class Robotapi extends plugin {
+    superIndex = (arr) => {
+        return {
+            name: 'xiuxian',
+            dsc: 'xiuxian',
+            event: 'message',
+            priority: 400,
+            rule: arr,
+        }
+    }
+}
+export const superIndex = (arr) => {
+    return {
+        name: 'xiuxian',
+        dsc: 'xiuxian',
+        event: 'message',
+        priority: 400,
+        rule: arr,
+    }
+}
