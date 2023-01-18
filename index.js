0b69667f388ef54741dc31a9128d3ca1f88f6a82
柠檬冲水 <916415899@qq.com>
2023-01-18 18:50:10 +0800

    优化

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import index from './model/robot/index.js';
 import schedule from './model/game/data/schedule.js';
 schedule.scheduleJobflie('xiuxian', '0 0 */1 * * ?');
-const xiuxain = await index.toindex({ indexName: 'apps', offArr: ['model', 'resources'] });
-const plugin = await index.toindex({ indexName: 'plugins', offArr: ['model', 'resources'] });
+const xiuxain = await index.toindex({ indexName: 'apps' });
+const plugin = await index.toindex({ indexName: 'plugins' });
 const apps = { ...xiuxain, ...plugin };
 logger.info(`Xiuxian-Plugin-Box[2023-1-16]`);
 export { apps }; 
--- model/robot/index.js
+++ model/robot/index.js
@@ -6,32 +6,29 @@ import { appname } from '../main.js'
  */
 class index {
   toindex = async (parameter) => {
-    const { indexName, offArr } = parameter
+    const { indexName } = parameter
     let filepath = `./plugins/${appname}/${indexName}`
     let apps = {}
     let name = []
     let newsum = []
     const travel = (dir, callback) => {
       fs.readdirSync(dir).forEach((file) => {
-        //屏蔽指定词
-        for (let item in offArr) {
-          let off = dir.search(item)
-          if (off != -1) {
-            return
+        let model = dir.search('model')
+        if (model == -1) {
+          let resources = dir.search('resources')
+          if (resources == -1) {
+            let temporary = file.search('.js')
+            if (temporary != -1) {
+              let y = file.replace('.js', '')
+              name.push(y)
+            }
+            var pathname = path.join(dir, file)
+            if (fs.statSync(pathname).isDirectory()) {
+              travel(pathname, callback)
+            } else {
+              callback(pathname)
+            }
           }
-        }
-        //js文件
-        let temporary = file.search('.js')
-        if (temporary != -1) {
-          //收集名字
-          let y = file.replace('.js', '')
-          name.push(y)
-        }
-        let pathname = path.join(dir, file)
-        if (fs.statSync(pathname).isDirectory()) {
-          travel(pathname, callback)
-        } else {
-          callback(pathname)
         }
       })
     }
