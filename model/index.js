e43b9481156d7b694b9ae8fb502e399525b1d62a
柠檬冲水 <916415899@qq.com>
2023-01-18 16:59:51 +0800

    优化

--- model/index.js
+++ model/index.js
@@ -5,42 +5,51 @@ import { appname } from './main.js'
  * 机器人入口
  */
 class index {
-  constructor() { }
-  toindex = async (input) => {
-    let filepath = `./plugins/${appname}/${input}`
+  toindex = async (parameter) => {
+    const { indexName, offArr } = parameter
+    let filepath = `./plugins/${appname}/${indexName}`
     let apps = {}
     let name = []
     let newsum = []
     const travel = (dir, callback) => {
       fs.readdirSync(dir).forEach((file) => {
-        let model = dir.search('model')
-        if (model == -1) {
-          let resources = dir.search('resources')
-          if (resources == -1) {
-            let temporary = file.search('.js')
-            if (temporary != -1) {
-              let y = file.replace('.js', '')
-              name.push(y)
-            }
-            var pathname = path.join(dir, file)
-            if (fs.statSync(pathname).isDirectory()) {
-              travel(pathname, callback)
-            } else {
-              callback(pathname)
-            }
+        //屏蔽指定词
+        for (let item in offArr) {
+          let off = dir.search(item)
+          if (off != -1) {
+            return
           }
+        }
+        //js文件
+        let temporary = file.search('.js')
+        if (temporary != -1) {
+          //收集名字
+          let y = file.replace('.js', '')
+          name.push(y)
+        }
+        let pathname = path.join(dir, file)
+        if (fs.statSync(pathname).isDirectory()) {
+          travel(pathname, callback)
+        } else {
+          callback(pathname)
         }
       })
     }
     travel(filepath, (pathname) => {
+      //屏蔽非js文件的目录
       let temporary = pathname.search('.js')
       if (temporary != -1) {
+        //收集目录
         newsum.push(pathname)
       }
     })
-    for (var j = 0 ;j < newsum.length ;j++) {
+    //合成引入
+    for (let j = 0; j < newsum.length; j++) {
+      //替换\\为/
       newsum[j] = newsum[j].replace(/\\/g, '/')
-      newsum[j] = newsum[j].replace(`plugins/${appname}` , '')
+      //替换前缀
+      newsum[j] = newsum[j].replace(`plugins/${appname}`, '')
+      //随深度的增加而增加
       apps[name[j]] = (await import(`..${newsum[j]}`))[name[j]]
     }
     return apps
