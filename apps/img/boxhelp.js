ab6534bd9282ef5618ca7b801bf2916025a611b4
柠檬冲水 <916415899@qq.com>
2023-01-19 01:07:18 +0800

    优化

--- apps/img/boxhelp.js
+++ apps/img/boxhelp.js
@@ -1,6 +1,7 @@
 import robotapi from "../../model/robot/api/api.js"
 import { superIndex } from "../../model/robot/api/api.js"
 import botapi from '../../model/robot/api/botapi.js'
+import gameApi from '../../model/api/api.js'
 export class boxhelp extends robotapi {
     constructor() {
         super(superIndex([
