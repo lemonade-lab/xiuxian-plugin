0b8e2c363e27cb1c46b037a596ca3f2a8505db13
柠檬冲水 <916415899@qq.com>
2023-01-19 10:11:31 +0800

    优化

--- model/game/public/map.js
+++ model/game/public/map.js
@@ -1,19 +1,17 @@
 import userData from '../user/data.js'
 class GameMap {
     //输入：模糊搜索名字并判断是否在此地
-    mapExistence = async (action, addressName) => {
+    mapExistence = async (paramater) => {
+        const { action, addressName } = paramater
         const point = await userData.listAction({ NAME: 'point', CHOICE: 'generate_position' })
-        let T = false
-        point.forEach((item, index, arr) => {
-            //存在模糊 
+        for (let item in point) {
             if (item.name.includes(addressName)) {
-                //且位置配对
                 if (action.x == item.x && action.y == item.y) {
-                    T = true
+                    return true
                 }
             }
-        })
-        return T
+        }
+        return false
     }
 
     //两者距离
