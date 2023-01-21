5d438d980f4bee4b6e9e16d3829130ba7e42ea10
柠檬冲水 <916415899@qq.com>
2023-01-21 10:32:11 +0800

    优化

--- model/robot/index.js
+++ model/robot/index.js
@@ -31,18 +31,18 @@ class index {
     })
     for (let item of sum) {
       let address = `../..${item.replace(/\\/g, '/').replace(`${firstName}`, '')}`
-      let allExport = (await import(address));
-      let keys = Object.keys(allExport);
+      let allExport = (await import(address))
+      let keys = Object.keys(allExport)
       keys.forEach((key) => {
         if (allExport[key].prototype) {
           if (apps.hasOwnProperty(key)) {
-            logger.info(`Template detection:已经存在class ${key}同名导出\n    ${address}`);
+            logger.info(`Template detection:已经存在class ${key}同名导出\n    ${address}`)
           }
-          apps[key] = allExport[key];
+          apps[key] = allExport[key]
         } else {
-          logger.info(`Template detection:存在非class属性${key}导出\n    ${address}`);
+          logger.info(`Template detection:存在非class属性${key}导出\n    ${address}`)
         }
-      });
+      })
     }
     return apps
   }
