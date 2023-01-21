107767bd1f24f1939a8038452d58cc6926517a39
柠檬冲水 <916415899@qq.com>
2023-01-21 10:31:13 +0800

    优化

--- model/robot/index.js
+++ model/robot/index.js
@@ -1,13 +1,11 @@
 import fs from 'node:fs'
 import path from 'path'
 import { appname } from '../main.js'
-/**
- * 机器人入口
- */
 class index {
   toindex = async (parameter) => {
     const { indexName } = parameter
-    const filepath = `./plugins/${appname}/${indexName}`
+    const firstName = `plugins/${appname}`
+    const filepath = `./${firstName}/${indexName}`
     let apps = {}
     const name = []
     const sum = []
@@ -15,8 +13,7 @@ class index {
       fs.readdirSync(dir).forEach((file) => {
         let temporary = file.search('.js')
         if (temporary != -1) {
-          let y = file.replace('.js', '')
-          name.push(y)
+          name.push(file.replace('.js', ''))
         }
         var pathname = path.join(dir, file)
         if (fs.statSync(pathname).isDirectory()) {
@@ -27,26 +24,23 @@ class index {
       })
     }
     travel(filepath, (path) => {
-      //屏蔽非js文件的目录
       let temporary = path.search('.js')
       if (temporary != -1) {
-        //收集目录
         sum.push(path)
       }
     })
     for (let item of sum) {
-      let address = item.replace(/\\/g, '/').replace(`plugins/${appname}`, '')
-      //随深度的增加而增加
-      let allExport = (await import(`../..${address}`));
+      let address = `../..${item.replace(/\\/g, '/').replace(`${firstName}`, '')}`
+      let allExport = (await import(address));
       let keys = Object.keys(allExport);
       keys.forEach((key) => {
         if (allExport[key].prototype) {
           if (apps.hasOwnProperty(key)) {
-            logger.info(`Template detection:已经存在class ${key}同名导出\n    ../..${address}`);
+            logger.info(`Template detection:已经存在class ${key}同名导出\n    ${address}`);
           }
           apps[key] = allExport[key];
         } else {
-          logger.info(`Template detection:存在非class属性${key}导出\n    ../..${address}`);
+          logger.info(`Template detection:存在非class属性${key}导出\n    ${address}`);
         }
       });
     }
