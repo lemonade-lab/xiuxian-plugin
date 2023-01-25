2b535e637c5e71454c9924a9e2b47fd66d46a951
柠檬冲水 <916415899@qq.com>
2023-01-25 14:26:15 +0800

    调整人物移动速度

---
+++ apps/place/boxmove.js
@@ -0,0 +1,11 @@
+import robotapi from "../../model/robot/api/api.js"
+import { superIndex } from "../../model/robot/api/api.js"
+import { BotApi } from '../../model/robot/api/botapi.js'
+import gameApi from '../../model/api/api.js'
+export class boxmove extends robotapi {
+    constructor() {
+        super(superIndex([
+            
+        ]))
+    }
+}
