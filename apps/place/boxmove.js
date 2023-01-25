8e54e102165b9afc7c6a2a34b6c23f5ee82a25cf
柠檬冲水 <916415899@qq.com>
2023-01-25 14:41:51 +0800

    增加人物移动

--- apps/place/boxmove.js
+++ apps/place/boxmove.js
@@ -5,7 +5,73 @@ import gameApi from '../../model/api/api.js'
 export class boxmove extends robotapi {
     constructor() {
         super(superIndex([
-            
+            {
+                reg: '^#mapw$',
+                fnc: 'mapW'
+            },
+            {
+                reg: '^#mapa$',
+                fnc: 'mapA'
+            },
+            {
+                reg: '^#maps$',
+                fnc: 'mapS'
+            },
+            {
+                reg: '^#mapd$',
+                fnc: 'mapD'
+            }
         ]))
     }
+    /**
+     * 前进
+     */
+    mapW=async(e)=>{
+        e.reply('待更新')
+        return
+    }
+    /**
+     * 向左移动
+     */
+    mapA=async(e)=>{
+        e.reply('待更新')
+        return
+    }
+    /**
+     * 后退
+     */
+    mapS=async(e)=>{
+        e.reply('待更新')
+        return
+    }
+    /**
+     * 向右移动
+     */
+    mapD=async(e)=>{
+        e.reply('待更新')
+        return
+    }
+
+    /**
+     * 走路消耗灵力（灵力？可能以后会影响元素攻击）
+     * （走到半路发现灵力没了，被人打一架。没有法术攻击。。美滋滋）
+     * 暴露啥了？
+     * 
+     */
+
+
+    /**
+     * 不同的地点发送的概率事件是不一样的
+     */
+
+    /**
+     * 不同境界走动的m是不一样的
+     */
+
+    /**
+     * 
+     */
+
+
 }
+
