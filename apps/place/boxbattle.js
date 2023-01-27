29065887f530183cfa316af612dc3c561890dc3a
柠檬冲水 <916415899@qq.com>
2023-01-27 15:44:26 +0800

    优化

--- apps/place/boxbattle.js
+++ apps/place/boxbattle.js
@@ -2,12 +2,17 @@ import robotapi from "../../model/robot/api/api.js"
 import { superIndex } from "../../model/robot/api/api.js"
 import { BotApi } from '../../model/robot/api/botapi.js'
 import gameApi from '../../model/api/api.js'
+import { GameApi } from "../../model/api/gameapi.js"
 export class boxbattle extends robotapi {
     constructor() {
         super(superIndex([
             {
                 reg: '^#决斗.*$',
                 fnc: 'duel'
+            },
+            {
+                reg: '^#打劫.*$',
+                fnc: 'robbery'
             },
             {
                 reg: '^#攻击.*$',
@@ -18,6 +23,87 @@ export class boxbattle extends robotapi {
                 fnc: 'handWashing'
             }
         ]))
+    }
+    robbery = async (e) => {
+        if (!e.isGroup) {
+            return
+        }
+        const UID = e.msg.replace('#打劫', '')
+        const find = GameApi.GameUser.existUser({ UID })
+        if (!find) {
+            e.reply('无存档')
+            return
+        }
+        if (!await GameApi.GameUser.existUserSatus({ UID: e.user_id })) {
+            e.reply('已死亡')
+            return
+        }
+        const { MSG } = await gameApi.Go({ UID: e.user_id })
+        if (MSG) {
+            e.reply(MSG)
+            return
+        }
+        const user = {
+            A: e.user_id,
+            B: UID,
+            C: 0,
+            QQ: 0,
+            p: Math.floor((Math.random() * (99 - 1) + 1))
+        }
+        const existB = await gameApi.existUserSatus({ UID: user['B'] })
+        if (!existB) {
+            e.reply('已死亡')
+            return
+        }
+        if (!user['B'] || user['B'] == user['A']) {
+            return
+        }
+        const actionA = await gameApi.userMsgAction({ NAME: user.A, CHOICE: 'user_action' })
+        const actionB = await gameApi.userMsgAction({ NAME: user.B, CHOICE: 'user_action' })
+        if (actionA.region != actionB.region) {
+            e.reply('此地未找到此人')
+            return
+        }
+        if (actionA.address == 1) {
+            e.reply('[修仙联盟]普通卫兵:城内不可出手!')
+            return
+        }
+        if (actionB.address == 1) {
+            e.reply('[修仙联盟]普通卫兵:城内不可出手!')
+            return
+        }
+        const CDID = '0'
+        const now_time = new Date().getTime()
+        const CDTime = gameApi.getConfig({ app: 'parameter', name: 'cooling' }).CD.Attack
+        const { CDMSG } = await gameApi.cooling({ UID: user.A, CDID })
+        if (CDMSG) {
+            e.reply(CDMSG)
+            return
+        }
+        user.QQ = await gameApi.battle({ e, A: user.A, B: user.B })
+        const Level = await gameApi.userMsgAction({ NAME: user.A, CHOICE: 'user_level' })
+        Level.prestige += 1
+        await gameApi.userMsgAction({ NAME: user.A, CHOICE: 'user_level', DATA: Level })
+        const LevelB = await gameApi.userMsgAction({ NAME: user.B, CHOICE: 'user_level' })
+        const MP = LevelB.prestige * 10 + Number(50)
+        if (user.p <= MP) {
+            if (user.QQ != user.A) {
+                user.C = user.A
+                user.A = user.B
+                user.B = user.C
+            }
+            let najieB = await gameApi.userMsgAction({ NAME: user.B, CHOICE: 'user_bag' })
+            if (najieB.thing.length != 0) {
+                const thing = await gameApi.Anyarray({ ARR: najieB.thing })
+                najieB.thing = najieB.thing.filter(item => item.name != thing.name)
+                await gameApi.userMsgAction({ NAME: user.B, CHOICE: 'user_bag', DATA: najieB })
+                await gameApi.userBag({ UID: user.A, name: thing.name, ACCOUNT: thing.acount })
+                e.reply(`${user.A}夺走了${thing.name}*${thing.acount}`)
+            }
+        }
+        await redis.set(`xiuxian:player:${user.A}:${CDID}`, now_time)
+        await redis.expire(`xiuxian:player:${user.A}:${CDID}`, CDTime * 60)
+        return
     }
     duel = async (e) => {
         if (!e.isGroup) {
