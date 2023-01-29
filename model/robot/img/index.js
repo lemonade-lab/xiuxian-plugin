da049cdde710faff676d04b1fd11fd1feb7480cd
柠檬冲水 <916415899@qq.com>
2023-01-29 14:23:14 +0800

    优化框架

--- model/robot/img/index.js
+++ model/robot/img/index.js
@@ -1,11 +1,20 @@
 import puppeteer from "../../../../../lib/puppeteer/puppeteer.js"
-import Show from "./show.js"
+import { appname } from "../../main.js"
+/**
+ * 中间返回show与yunzai的图片方法进行对接
+ */
 class ImgIndex {
-    showPuppeteer = async (parameter) => {
-        const { path, name, data } = parameter
-        const mydata = await Show.getData({
-            path, name, data
-        })
+    showPuppeteer = async ({ path, name, data }) => {
+        const mydata = {
+            /** 文件名 */
+            saveId: name,
+            /** 相对路径 */
+            tplFile: `./plugins/${appname}/resources/html/${path}/${name}.html`,
+            /** 绝对路径 */
+            pluResPath: `${process.cwd().replace(/\\/g, '/')}/plugins/${appname}/resources/`,
+            /** 数据 */
+            ...data,
+        }
         const img = await puppeteer.screenshot(name, {
             ...mydata,
         })
