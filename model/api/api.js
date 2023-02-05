3774ef3e84c34e11e4ba9f3ff1dedeff9e1d785b
柠檬冲水 <916415899@qq.com>
2023-02-05 18:49:22 +0800

    优化框架

--- model/api/api.js
+++ model/api/api.js
@@ -1,4 +1,6 @@
+/*yunzai*/
 import plugin from '../../../../lib/plugins/plugin.js'
+/*bot*/
 import { BotApi } from './botapi.js'
 import { GameApi } from './gameapi.js'
 export { BotApi, GameApi, plugin }
