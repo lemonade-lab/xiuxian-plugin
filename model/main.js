2749362dc127eec7f3955095a6e369562df23d9d
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-02-09 03:24:14 +0000

    插件名更改
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- model/main.js
+++ model/main.js
@@ -1,5 +1,5 @@
 import path from 'path'
 /**插件名*/
-export const appname = 'Xiuxian-Plugin-Box'
+export const appname = 'xiuxian-plugin'
 /*插件绝对路径 */
 export const __dirname = `${path.resolve()}${path.sep}plugins${path.sep}${appname}`
