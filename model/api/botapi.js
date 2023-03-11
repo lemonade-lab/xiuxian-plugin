b416a6aec97f3268ebd42b44298416b7a1796215
柠檬冲水 <916415899@qq.com>
2023-03-11 19:33:33 +0800

    优化

--- model/api/botapi.js
+++ model/api/botapi.js
@@ -3,6 +3,6 @@ import ImgCache from '../robot/img/cache.js'
 import ImgHelp from '../robot/img/help.js'
 import User from '../robot/user/action.js'
 import Exec from '../robot/exec/exex.js'
-import Index from '../robot/index.js'
+import { toIndex } from '../robot/index.js'
 import { segment } from 'oicq'
-export const BotApi = { ImgIndex, Index, ImgCache, ImgHelp, User, Exec, segment }
+export const BotApi = { ImgIndex, toIndex, ImgCache, ImgHelp, User, Exec, segment }
