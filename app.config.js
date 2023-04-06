c133c8010f724cc8f41172f4fe5e9966a7b54745
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-04-06 04:24:59 +0000

    update app.config.js.
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 /**自定义全局插件名*/
-export const AppName = 'xiuxian-plugin';
+export const AppName = 'xiuxian@1.3.0';
 
 export const ThePath = `${path.resolve().replace(/\\/g, '/')}`;
 /**自定义全局插件绝对路径*/
