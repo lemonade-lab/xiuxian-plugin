a07588c9ed9cee279564456c3c854babf95d281a
学做菜的瓜瓜皮 <11319135+cxcx123456@user.noreply.gitee.com>
2023-05-24 02:51:18 +0000

    修复卡死
    Signed-off-by: 学做菜的瓜瓜皮 <11319135+cxcx123456@user.noreply.gitee.com>

--- model/xiuxian.js
+++ model/xiuxian.js
@@ -196,7 +196,7 @@ export async function Read_equipment(usr_qq) {
     return data
   })
   //将字符串数据转变成数组格式
-  equipment = JSON.parse(decodeURIComponent(equipment))
+  equipment = JSON.parse(equipment)
   return equipment
 }
 
@@ -256,7 +256,7 @@ export async function Read_najie(usr_qq) {
   })
   //将字符串数据转变成数组格式
   try {
-    najie = JSON.parse(decodeURIComponent(najie))
+    najie = JSON.parse(najie)
   } catch {
     //转换不了，纳戒错误
     await fixed(usr_qq)
@@ -2311,7 +2311,7 @@ export async function Read_danyao(usr_qq) {
     return data
   })
   //将字符串数据转变成数组格式
-  danyao = JSON.parse(decodeURIComponent(danyao))
+  danyao = JSON.parse(danyao)
   return danyao
 }
 
@@ -2334,7 +2334,7 @@ export async function Read_temp() {
     return data
   })
   //将字符串数据转变成数组格式
-  temp = JSON.parse(decodeURIComponent(temp))
+  temp = JSON.parse(temp)
   return temp
 }
 
