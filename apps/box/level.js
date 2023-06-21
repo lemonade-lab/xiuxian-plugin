17320c8c07ecbabcd7cff1ae71b1126979b80ac0
柠檬冲水 <916415899@qq.com>
2023-06-21 15:36:14 +0800

    修复隐藏宗门创建

--- apps/box/level.js
+++ apps/box/level.js
@@ -12,13 +12,23 @@ export class BoxLevel extends plugin {
   async levelUp(e) {
     if (!this.verify(e)) return false
     const UID = e.user_id
-    if (!GameApi.Player.getUserLifeSatus(e.user_id)) {
+    if (!GameApi.Player.getUserLifeSatus(UID)) {
       e.reply('已仙鹤')
+      return false
+    }
+    const CDID = 6
+    const { state: coolingState, msg: coolingMsg } = GameApi.Burial.cooling(e.user_id, CDID)
+    if (coolingState == 4001) {
+      e.reply(coolingMsg)
       return false
     }
     const res = GameApi.Levels.enhanceRealm(UID, 0)
     const { msg } = res
     e.reply(msg)
+    const nowTime = new Date().getTime()
+    const cf = GameApi.Defset.getConfig({ name: 'cooling' })
+    const CDTime = cf.CD.Attack ? cf.CD.Attack : 5
+    GameApi.Burial.set(UID, CDID, nowTime, CDTime)
     return false
   }
 
@@ -29,9 +39,19 @@ export class BoxLevel extends plugin {
       e.reply('已仙鹤')
       return false
     }
+    const CDID = 7
+    const { state: coolingState, msg: coolingMsg } = GameApi.Burial.cooling(e.user_id, CDID)
+    if (coolingState == 4001) {
+      e.reply(coolingMsg)
+      return false
+    }
     const res = GameApi.Levels.enhanceRealm(UID, 1)
     const { msg } = res
     e.reply(msg)
+    const nowTime = new Date().getTime()
+    const cf = GameApi.Defset.getConfig({ name: 'cooling' })
+    const CDTime = cf.CD.Attack ? cf.CD.Attack : 5
+    GameApi.Burial.set(UID, CDID, nowTime, CDTime)
     return false
   }
 }
