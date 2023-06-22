18c6db25ce71324d0f1651f2d21aa6fc93b8279a
柠檬冲水 <916415899@qq.com>
2023-06-22 10:25:25 +0800

    优化背包算法

--- model/game/box/bag.js
+++ model/game/box/bag.js
@@ -6,101 +6,29 @@ class Bag {
    * @returns
    */
   addBagThing({ UID, name, ACCOUNT }) {
-    const thing = Listdata.searchAllThing('name', name)
-    if (thing) {
-      let bag = Listdata.controlAction({ CHOICE: 'playerBag', NAME: UID })
-      bag = this.addBagThingAction({
-        BAG: bag,
-        THING: thing,
-        ACCOUNT: Number(ACCOUNT)
-      })
-      Listdata.controlAction({ CHOICE: 'playerBag', NAME: UID, DATA: bag })
-      return true
-    }
-    return false
-  }
-
-  /**
-   * tudp
-   * 材料仓库物品增减
-   * @param { UID, name, ACCOUNT } param0
-   * @returns
-   */
-  updateMaterial({ UID, name, ACCOUNT }) {
-    /**
-     * 搜索仓库物品
-     */
-    const thing = Listdata.searchThing({
-      CHOICE: 'fixed_material',
-      NAME: 'MaterialGUIDe',
-      condition: 'name',
-      name
-    })
-    if (thing) {
-      let bag = Listdata.controlAction({
-        CHOICE: 'playerMaterial',
-        NAME: UID
-      })
-      bag = this.updateMaterialAction({
-        BAG: bag,
-        THING: thing,
-        ACCOUNT: Number(ACCOUNT)
-      })
-      Listdata.controlAction({
-        CHOICE: 'playerMaterial',
-        NAME: UID,
-        DATA: bag
-      })
-      return true
-    }
-    return false
-  }
-
-  /**
-   * tudo
-   * @param {*} parameter
-   * @returns
-   */
-  updateMaterialAction(parameter) {
-    let { BAG, THING, ACCOUNT } = parameter
-    const thing = BAG.find((item) => item.id == THING.id)
-    if (thing) {
-      let acount = Number(thing.acount) + Number(ACCOUNT)
-      if (Number(acount) < 1) {
-        BAG = BAG.filter((item) => item.id != THING.id)
-      } else {
-        BAG.find((item) => item.id == THING.id).acount = Number(acount)
-      }
-      return BAG
-    } else {
+    const THING = Listdata.searchAllThing('name', name)
+    if (!THING) return false
+    /* 两者匹配 */
+    let BAG = Listdata.controlAction({ CHOICE: 'playerBag', NAME: UID })
+    const FINDDATA = BAG.thing.find((item) => item.id == THING.id)
+    /* 匹配成功 */
+    if (!FINDDATA) {
+      // 直接推送
       THING.acount = Number(ACCOUNT)
       BAG.thing.push(THING)
-      return BAG
-    }
-  }
-
-  /**
-   * tudo
-   * 给储物袋添加物品
-   * @param { BAG, THING, ACCOUNT } param0
-   * @returns
-   */
-  addBagThingAction(parameter) {
-    const { BAG, THING, ACCOUNT } = parameter
-    const thing = BAG.thing.find((item) => item.id == THING.id)
-    if (thing) {
-      let acount = Number(thing.acount) + Number(ACCOUNT)
-      if (Number(acount) < 1) {
-        BAG.thing = BAG.thing.filter((item) => item.id != THING.id)
-      } else {
-        BAG.thing.find((item) => item.id == THING.id).acount = Number(acount)
-      }
-      return BAG
     } else {
-      THING.acount = Number(ACCOUNT)
-      BAG.thing.push(THING)
-      return BAG
+      // 删除物品
+      BAG.thing = BAG.thing.filter((item) => item.id != THING.id)
+      let SIZE = Number(FINDDATA.acount) + Number(ACCOUNT)
+      // 如果小了,跳过
+      // 如果大了,重新放回去
+      if (SIZE > 1) {
+        THING.acount = SIZE
+        BAG.thing.push(THING)
+      }
     }
+    Listdata.controlAction({ CHOICE: 'playerBag', NAME: UID, DATA: BAG })
+    return true
   }
 
   /**
@@ -108,11 +36,9 @@ class Bag {
    * @param { UID, name } param0
    * @returns 返回该物品
    */
-
   searchBagByName({ UID, name }) {
     const bag = Listdata.controlAction({ CHOICE: 'playerBag', NAME: UID })
     return bag.thing.find((item) => item.name == name)
   }
 }
-
 export default new Bag()
