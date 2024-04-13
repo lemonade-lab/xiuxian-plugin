2bd6db7e6a969817672518bb929c715301d37a28
ningmengchongshui <916415899@qq.com>
2024-04-13 21:07:26 +0800

    feat: 优化登录界面

--- src-vite/view/login/App.tsx
+++ src-vite/view/login/App.tsx
@@ -7,6 +7,9 @@ import '../styles.css'
 export default function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [userName, setUserName] = useState('')
+  const [passWord, setPassWord] = useState('')
+  const [show, setShow] = useState(true)
 
   const navigate = useNavigate()
 
@@ -55,39 +58,112 @@ export default function App() {
   }
 
   const handleKeyDown = (key) => {
-    if (key === 'Enter') handleLogin()
+    if (key === 'Enter') {
+      if (show) {
+        handleLogin()
+      } else {
+        handleLogn()
+      }
+    }
+  }
+
+  const handleLogn = () => {
+    console.log('handleLogon')
+
+    // 构建请求数据
+    const tData = {
+      username: userName,
+      password: passWord
+    }
+
+    const uArr = tData.username.split('')
+    if (uArr.length < 6 || uArr.length > 18) {
+      alert('账号或密码非法长度！')
+      return
+    }
+    const pArr = tData.password.split('')
+    if (pArr.length < 6 || pArr.length > 18) {
+      alert('账号或密码非法长度！')
+      return
+    }
+
+    client
+      .logon(tData)
+      .then((res) => {
+        console.log('res', res)
+        if (res.code == 5000) {
+          alert('服务器错误，请稍后重试。。。')
+        } else if (res.code == 4000) {
+          alert(res.msg)
+        } else if (res.code == 2000) {
+          alert('注册成功，请前往登录')
+        } else {
+          alert('未知错误')
+        }
+      })
+      .catch(() => {
+        alert('服务器繁忙，请稍后重试。。。')
+      })
   }
 
   return (
     <div className="com">
       <main className="comMain">
-        {/* <img className="comMainLogo" src={logoSvg} /> */}
-        <div className="comMainLogin">
-          <div className="header">
-            <h1 className="title">欢迎使用 修仙管理系统</h1>
+        {show ? (
+          <div className="comMainLogin">
+            <div className="header">
+              <h1 className="title">欢迎使用修仙管理系统</h1>
+            </div>
+            <input
+              type="text"
+              className="input"
+              placeholder="用户名"
+              onChange={(e) => setUsername(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e.key)}
+            />
+            <input
+              type="password"
+              className="input"
+              placeholder="密码"
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e.key)}
+            />
+            <button className="loginButton" onClick={handleLogin}>
+              登录
+            </button>
+            <div className="registerLink">
+              还没有账号？
+              <a onClick={() => setShow(!show)}>前往注册</a>
+            </div>
           </div>
-          <input
-            type="text"
-            className="input"
-            placeholder="用户名"
-            onChange={(e) => setUsername(e.target.value)}
-            onKeyDown={(e) => handleKeyDown(e.key)}
-          />
-          <input
-            type="password"
-            className="input"
-            placeholder="密码"
-            onChange={(e) => setPassword(e.target.value)}
-            onKeyDown={(e) => handleKeyDown(e.key)}
-          />
-          <button className="loginButton" onClick={handleLogin}>
-            登录
-          </button>
-          <div className="registerLink">
-            还没有账号？
-            <a href="/logon">前往注册</a>
+        ) : (
+          <div className="comMainLogin">
+            <div className="header">
+              <h1 className="title">正在注册修仙账户</h1>
+            </div>
+            <input
+              type="text"
+              className="input"
+              placeholder="用户名"
+              onChange={(e) => setUserName(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e.key)}
+            />
+            <input
+              type="password"
+              className="input"
+              placeholder="密码"
+              onChange={(e) => setPassWord(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e.key)}
+            />
+            <button className="loginButton" onClick={handleLogn}>
+              注册
+            </button>
+            <div className="registerLink">
+              已经拥有账户？
+              <a onClick={() => setShow(!show)}>前往登录</a>
+            </div>
           </div>
-        </div>
+        )}
       </main>
     </div>
   )
