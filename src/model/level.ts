21fb9e018e399a56cd2540dbe26ccd4238c7b82e
ningmengchongshui <916415899@qq.com>
2024-06-03 18:20:22 +0800

    fix: 攻击力计算错误

--- src/model/level.ts
+++ src/model/level.ts
@@ -10,19 +10,19 @@ function getAttackById(id: number) {
     return BaseLevel.attack
   } else if (id < 13) {
     return BaseLevel.attack * (id + 3) * 0.3
-  } else if (13 <= id && id < 17) {
+  } else if (13 >= id && id < 17) {
     return BaseLevel.attack * id * 2 * 0.4
-  } else if (17 <= id && id < 21) {
+  } else if (17 >= id && id < 21) {
     return BaseLevel.attack * id * 3 * 0.5
-  } else if (21 <= id && id < 25) {
+  } else if (21 >= id && id < 25) {
     return BaseLevel.attack * id * 4 * 0.6
-  } else if (25 <= id && id < 29) {
+  } else if (25 >= id && id < 29) {
     return BaseLevel.attack * id * 5 * 0.7
-  } else if (29 <= id && id < 33) {
+  } else if (29 >= id && id < 33) {
     return BaseLevel.attack * id * 6 * 0.8
-  } else if (33 <= id && id < 37) {
+  } else if (33 >= id && id < 37) {
     return BaseLevel.attack * id * 7 * 0.9
-  } else if (37 <= id && id < 46) {
+  } else if (37 >= id && id < 46) {
     return BaseLevel.attack * id * 10 * 1.0
   } else {
     return BaseLevel.attack * id * 10 * 1.2
