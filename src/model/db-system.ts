bf5f1b793f4860cb53b245c254b3f665547aaa0b
ningmengchongshui <916415899@qq.com>
2024-06-03 18:05:25 +0800

    feat: 新数据系统

--- src/model/db-system.ts
+++ src/model/db-system.ts
@@ -1,90 +1,9 @@
-/**
- * ****************
- * 基于JSON而设计的数据库系统
- * ****************
- */
-import { existsSync, mkdirSync, promises, readFileSync, writeFile } from 'fs'
-import { extname, join } from 'path'
 import { UserMessageBase } from './base'
 
-/**
- * 默认索引为 id
- *
- * 如果经常以某一值为搜索条件
- * 必须建立索引
- * 也就是关系映射
- *
- */
-
 export class MyData<T> {
-  #dir = process.cwd()
+  #key = 'xiuxian'
 
-  /**
-   * 数据库路径初始化
-   * @param dir
-   */
-  constructor(dir: string) {
-    const d = join(process.cwd(), dir)
-    this.#dir = d
-    mkdirSync(this.#dir, {
-      recursive: true
-    })
-  }
-
-  /**
-   * 是否存在
-   * @param key
-   * @returns
-   */
-  exists(key: string) {
-    const dir = join(this.#dir, `${key}.json`)
-    return existsSync(dir)
-  }
-
-  /**
-   * 得到所有用户数据
-   */
-  async findAll() {
-    return new Promise((resolve, reject) => {
-      promises
-        .readdir(this.#dir)
-        .then(async (files) => {
-          const data: T[] = []
-          for (const file of files) {
-            const filePath = join(this.#dir, file)
-            const stats = await promises.stat(filePath)
-            if (stats.isFile() && extname(filePath) === '.json') {
-              const d = readFileSync(filePath, 'utf-8')
-              data.push(JSON.parse(d) as T)
-            }
-          }
-          resolve(data)
-        })
-        .catch(reject)
-    })
-  }
-
-  /**
-   * 数据长度
-   */
-  async count() {
-    let c = 0
-    try {
-      const files = await promises.readdir(this.#dir)
-      for (const file of files) {
-        const filePath = join(this.#dir, file)
-        const stats = await promises.stat(filePath)
-        if (stats.isFile() && extname(filePath) === '.json') {
-          c++
-        }
-      }
-      return c
-    } catch (error) {
-      console.error('Error:', error)
-      // 返回-1表示出现错误
-      return -1
-    }
-  }
+  #init: T = null
 
   /**
    *
@@ -92,28 +11,69 @@ export class MyData<T> {
   lock = new Map()
 
   /**
+   * 数据库路径初始化
+   * @param dir
+   */
+  constructor(key: string, init: T) {
+    this.#key = key
+    this.#init = init
+  }
+
+  /**
+   *
+   * @param key
+   */
+  getKey(key: string | number) {
+    return `${this.#key}:${key}`
+  }
+
+  /**
+   * 是否存在
+   * @param key
+   * @returns
+   */
+  async exists(key: string | number) {
+    return redis.exists(this.getKey(key)).then((res) => res === 1)
+  }
+
+  /**
+   * 得到所有用户数据
+   */
+  async findAll() {
+    return new Promise(() => {
+      //
+    })
+  }
+
+  /**
+   * 数据长度
+   */
+  async count() {
+    //
+  }
+
+  /**
    * 读取数据
    */
   findOne(key: string): Promise<T | false> {
     return new Promise((resolve, reject) => {
-      if (!this.lock.has(key)) return reject(false)
+      if (this.lock.has(key)) return reject(false)
       // 锁住行为
       this.lock.set(key, Date.now)
-      const dir = join(this.#dir, `${key}.json`)
-      if (existsSync(dir)) {
-        try {
-          const data = readFileSync(dir, 'utf-8')
+
+      // for in 存储
+
+      redis
+        .get(this.getKey(key))
+        .then((res) => {
           this.lock.delete(key)
-          resolve(JSON.parse(data) as T)
-        } catch (err) {
+          resolve(JSON.parse(res) as T)
+        })
+        .catch((err) => {
+          this.lock.delete(key)
           console.error(err)
-          this.lock.delete(key)
-          reject(err)
-        }
-      } else {
-        this.lock.delete(key)
-        reject(false)
-      }
+          reject(false)
+        })
     })
   }
 
@@ -123,39 +83,54 @@ export class MyData<T> {
    * @param data
    * @returns
    */
-  create(key: string, data: T) {
+  create(key: string, data: T): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      if (!this.lock.has(key)) {
+      if (this.lock.has(key)) {
         reject(false)
         return
       }
-      const dir = join(this.#dir, `${key}.json`)
-      writeFile(dir, JSON.stringify(data), 'utf-8', (err) => {
-        if (err) {
-          reject(err)
-        } else {
+      // 锁住行为
+      this.lock.set(key, Date.now)
+
+      // for in 读取
+
+      // 开始写入数据
+      redis
+        .set('x', JSON.stringify(data))
+        .then(() => {
+          this.lock.delete(key)
           resolve(true)
-        }
-      })
+        })
+        .catch((err) => {
+          this.lock.delete(key)
+          console.error(err)
+          reject(false)
+        })
     })
+  }
+
+  /**
+   * 初始化数据
+   * @param key
+   * @returns
+   */
+  init(key: string | number) {
+    return this.create(this.getKey(key), this.#init)
   }
 
   /**
    * 更新数据的指定key
    */
   async update(key: string, data: T) {
-    const d = await this.findOne(key)
+    const d = await this.findOne(this.getKey(key))
     if (!d) return false
-    const n = {
+    return this.create(this.getKey(key), {
       ...d,
       ...data
-    }
-    return this.create(key, n)
+    })
   }
 
   //
 }
 
-export const DB = new MyData<typeof UserMessageBase>('/data')
-
-// const d = DB.findOne('')
+export const DB = new MyData('xiuxian:player', UserMessageBase)
