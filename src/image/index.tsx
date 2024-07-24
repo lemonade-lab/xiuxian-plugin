7483689a84c0281301296ad72a04ba679353fdd9
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-07-24 14:50:39 +0800

    fix

--- src/image/index.tsx
+++ src/image/index.tsx
@@ -23,10 +23,7 @@ const Paths = {
 const RootLink = () => {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href={require('../../public/output.css')}
-      ></link>
+      <link rel="stylesheet" href={require('../../public/output.css')}></link>
       <link
         rel="stylesheet"
         href={require('../../resources/css/root.css')}
